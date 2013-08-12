1ce80f8107676c93efa6914859da7d7c91d01ffa
sandlbn <marcin@spoczynski.com>
2013-08-13 00:44:42 +0200

    Some models

--- django_theatre/models.py
+++ django_theatre/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.translation import ugettext as _
+from django.utils import timezone
 
-# Create your models here.
+'''
+Performance description 
+'''
+class Performance(models.Model):
+    name = models.CharField(max_length=255, verbose_name=_(u'Name'), null=False, blank=False)
+    description = models.TextField(verbose_name=_(u'Description'))
+    created = models.DateTimeField(verbose_name=_('Date published'),default=timezone.now)
+    edited = models.DateTimeField(verbose_name=_('Date edited'))
+    def __unicode__(self):
+        return self.name
+    
+'''
+Performance Schedule 
+'''  
+class PerformanceTime(models.Model):
+    performance = models.ForeignKey(Performance, verbose_name=_(u'Performance'))
+    performance_date = models.DateTimeField(verbose_name=_(u'Time'))
+    published = models.BooleanField(verbose_name=_(u'Is Published ?'))
+    
+    def __unicode__(self):
+        return u'{0} {1}'.format(self.performance.name, self.performance_date)
