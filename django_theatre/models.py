bfe699c5088d313252fdd387ab48e562c77276b8
sandlbn <marcin@spoczynski.com>
2013-08-13 01:27:38 +0200

    pep8 style

--- django_theatre/models.py
+++ django_theatre/models.py
@@ -2,24 +2,29 @@ from django.db import models
 from django.utils.translation import ugettext as _
 from django.utils import timezone
 
-'''
-Performance description 
-'''
+'''Performance description '''
+
+
 class Performance(models.Model):
-    name = models.CharField(max_length=255, verbose_name=_(u'Name'), null=False, blank=False)
+    name = models.CharField(max_length=255, verbose_name=_(u'Name'),
+                             null=False, blank=False)
     description = models.TextField(verbose_name=_(u'Description'))
-    created = models.DateTimeField(verbose_name=_('Date published'),default=timezone.now)
+    created = models.DateTimeField(verbose_name=_('Date published'),
+                                   default=timezone.now)
     edited = models.DateTimeField(verbose_name=_('Date edited'))
+
     def __unicode__(self):
         return self.name
-    
-'''
-Performance Schedule 
-'''  
+
+
+'''Performance Schedule '''
+
+
 class PerformanceTime(models.Model):
-    performance = models.ForeignKey(Performance, verbose_name=_(u'Performance'))
+    performance = models.ForeignKey(Performance,
+                                    verbose_name=_(u'Performance'))
     performance_date = models.DateTimeField(verbose_name=_(u'Time'))
     published = models.BooleanField(verbose_name=_(u'Is Published ?'))
-    
+
     def __unicode__(self):
         return u'{0} {1}'.format(self.performance.name, self.performance_date)
