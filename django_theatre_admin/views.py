de591faf64952525dbd0d7e882806cb00016d445
sandlbn <marcin@spoczynski.com>
2013-08-16 13:01:56 +0200

    Skeleton of django_theatre_admin

---
+++ django_theatre_admin/views.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+__author__ = 'sandlbn'
+from django_theatre.models import Performance, PerformanceTime
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template.context import RequestContext
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, TemplateView
+from django.utils.datetime_safe import datetime
+from django.views.generic.edit import CreateView, UpdateView,DeleteView, FormMixin
+from braces.views import LoginRequiredMixin,CsrfExemptMixin
+
+ 
+class PerformancesView(ListView):
+    ''' Performaces List '''
+    queryset = Performance.objects.filter().order_by('-id')
+    context_object_name = 'performances'
+    paginate_by = 20
+
+
+class PerformanceTimesView(ListView):
+    ''' Performaces Time List '''
+
+    context_object_name = 'performances_time'
+    paginate_by = 20
+
+    def get_queryset(self, *args, **kwargs):
+        self.performance = get_object_or_404(
+                            Performance,
+                            pk=self.kwargs.get('pk')
+                            )
+        return PerformanceTime.objects.filter(
+                performance=self.performance
+                ).order_by('-date')
