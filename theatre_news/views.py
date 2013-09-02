6506da363d1faff8d8bb1710be6f9b5d4da6d990
sandlbn <marcin@spoczynski.com>
2013-09-03 00:12:13 +0200

    theatre_news templates

--- theatre_news/views.py
+++ theatre_news/views.py
@@ -7,20 +7,25 @@ from django.views.generic import DetailView
 from django.views.generic import CreateView
 from django.views.generic import UpdateView
 from django.views.generic import DeleteView
+from django.core.urlresolvers import reverse_lazy
 
 
 class NewsCreateView(CreateView):
 
     model = News
+    template_name_suffix = '_create_form'
 
 
 class NewsUpdateView(UpdateView):
 
     model = News
+    template_name_suffix = '_update_form'
+
 
 class NewsDeleteView(DeleteView):
 
     model = News
+    success_url = reverse_lazy('admin-news-list')
 
 class NewsListView(ListView):
 
@@ -34,18 +39,25 @@ class NewsDetailView(DetailView):
 class StaticPageCreateView(CreateView):
 
     model = StaticPage
+    template_name_suffix = '_create_form'
+
 
 class StaticPageUpdateView(UpdateView):
 
     model = StaticPage
+    template_name_suffix = '_update_form'
+
 
 class StaticPageDeleteView(DeleteView):
 
     model = StaticPage
+    success_url = reverse_lazy('admin-news-list')
+
 
 class StaticPageListView(ListView):
 
     model = StaticPage
+
 
 class StaticPageDetailView(DetailView):
 
