7920c58e8d1c90ca1a5310bbaa9931483d4f4e67
sandlbn <marcin@spoczynski.com>
2013-09-03 21:35:21 +0200

    next templates

--- theatre_news/views.py
+++ theatre_news/views.py
@@ -7,67 +7,72 @@ from django.views.generic import DetailView
 from django.views.generic import CreateView
 from django.views.generic import UpdateView
 from django.views.generic import DeleteView
+from braces.views import StaffuserRequiredMixin
 from django.core.urlresolvers import reverse_lazy
 
 
-class NewsCreateView(CreateView):
+class NewsCreateView(CreateView, StaffuserRequiredMixin):
 
     model = News
-    template_name = 'admin/news_create_form.html'
+    template_name = 'theatre_news/backend/news_create_form.html'
 
 
-class NewsUpdateView(UpdateView):
+class NewsUpdateView(UpdateView, StaffuserRequiredMixin):
 
     model = News
-    template_name = 'admin/news_update_form.html'
+    template_name = 'theatre_news/backend/news_update_form.html'
 
 
-class NewsDeleteView(DeleteView):
+class NewsDeleteView(DeleteView,StaffuserRequiredMixin):
 
     model = News
     success_url = reverse_lazy('admin-news-list')
-    template_name = 'admin/news_confirm_delete_form.html'
+    template_name = 'theatre_news/backend/news_confirm_delete_form.html'
 
 
 class NewsListView(ListView):
 
     model = News
-    template_name = 'admin/news_list.html'
+    template_name = 'theatre_news/frontend/news_list.html'
 
+class NewsListBackendView(ListView, StaffuserRequiredMixin):
+
+    model = News
+    template_name = 'theatre_news/backend/news_list.html'
 
 class NewsDetailView(DetailView):
 
     model = News
-    template_name = 'frontend/news_detail.html'
+    template_name = 'theatre_news/frontend/news_detail.html'
 
 
 class StaticPageCreateView(CreateView):
 
     model = StaticPage
-    template_name = 'admin/static_page_create_form.html'
+    template_name = 'theatre_news/backend/static_page_create_form.html'
 
 
 class StaticPageUpdateView(UpdateView):
 
     model = StaticPage
-    template_name = 'admin/static_page_update_form.html'
+    template_name = 'theatre_news/backend/static_page_update_form.html'
 
 
 class StaticPageDeleteView(DeleteView):
 
     model = StaticPage
     success_url = reverse_lazy('admin-news-list')
-    template_name = 'admin/static_page_confirm_delete.html'
+    template_name = 'theatre_news/backend/static_page_confirm_delete.html'
 
 
 class StaticPageListView(ListView):
 
     model = StaticPage
-    template_name = 'admin/static_page_list.html'
+    template_name = 'theatre_news/backend/static_page_list.html'
 
 
 class StaticPageDetailView(DetailView):
 
     model = StaticPage
-    template_name = 'frontend/static_page_detail.html'
+    template_name = 'theatre_news/frontend/static_page_detail.html'
 
