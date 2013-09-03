39db0ae0864c3bfd3bb71ca0f7e03138444a41ea
sandlbn <marcin@spoczynski.com>
2013-09-03 23:49:47 +0200

    Some new templates

--- theatre_news/urls.py
+++ theatre_news/urls.py
@@ -5,6 +5,14 @@ __author__ = 'sandlbn'
 from django.conf.urls import patterns, url
 from .views import NewsDetailView
 from .views import StaticPageDetailView
+from .views import NewsCreateView
+from .views import NewsUpdateView
+from .views import NewsDeleteView
+from .views import NewsListBackendView
+from .views import StaticPageCreateView
+from .views import StaticPageUpdateView
+from .views import StaticPageDeleteView
+from .views import StaticPageListView
 
 
 urlpatterns = patterns(
@@ -20,3 +28,48 @@ urlpatterns = patterns(
         name='news-detail'
     )
 )
+
+urlpatterns += patterns(
+    '.views',
+    url(
+        r'^backend/static/list/$',
+        StaticPageListView.as_view(),
+        name='backend-static-list'
+    ),
+    url(
+        r'^backend/static/create/$',
+        StaticPageCreateView.as_view(),
+        name='backend-static-create'
+    ),
+    url(
+        r'^backend/static/update/(?P<pk>\d)$',
+        StaticPageUpdateView.as_view(),
+        name='backend-static-create'
+    ),
+    url(
+        r'^backend/static/delete/(?P<pk>\d)$',
+        StaticPageDeleteView.as_view(),
+        name='backend-static-delete'
+    ),
+    url(
+        r'^backend/news/list/$',
+        NewsListBackendView.as_view(),
+        name='backend-news-list'
+    ),
+    url(
+        r'^backend/news/create/$',
+        NewsCreateView.as_view(),
+        name='backend-news-create'
+    ),
+    url(
+        r'^backend/news/update/(?P<pk>\d)$',
+        NewsUpdateView.as_view(),
+        name='backend-news-create'
+    ),
+    url(
+        r'^backend/news/delete/(?P<pk>\d)$',
+        NewsDeleteView.as_view(),
+        name='backend-news-delete'
+    ),
+)
+
