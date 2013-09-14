b04abc535913826daa1df93719da91f2606ad7d5
sandlbn <marcin@spoczynski.com>
2013-09-15 00:32:24 +0200

    reservation module

--- theatre_booking/models.py
+++ theatre_booking/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from theatre_core.models import Timestampable
 
-# Create your models here.
+class Chair(models.Model):
+
+    sys_number = models.IntegerField(
+        max_length=2,
+        verbose_name=_(u'Chair system number')
+    )
+    chair_number = models.IntegerField(max_lenght=3, verbose_name=u'Number')
+    chair_row = models.IntegerField(max_lenght=3, verbose_name=u'Row Number')
+
+    def __unicode__(self):
+        return self.chair_number
+
+
+class Reservation(TimeStampable):
+    user = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return user
